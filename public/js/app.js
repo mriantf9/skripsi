44661ad4ffcf6aad1e0fd48cf3669dc76449ed15
root <root@mrtg-skripsi.asia-southeast2-a.c.certain-benefit-297203.internal>
2020-12-02 16:23:14 +0000

    add .htaccess

--- public/js/app.js
+++ public/js/app.js
@@ -18093,8 +18093,8 @@ try {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\LARAVEL\skripsi\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\LARAVEL\skripsi\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/html/skripsi/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/html/skripsi/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
